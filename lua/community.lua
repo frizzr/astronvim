29e15a77c3fb25bc5821ac6f64c6e2f36b1775a1
Russ Frizzell-Carlton <frizzr@gmail.com>
2025-01-01 09:09:45 -0500

    Add go and python support

--- lua/community.lua
+++ lua/community.lua
@@ -9,5 +9,7 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.python" },
   -- import/override with your plugins folder
 }
